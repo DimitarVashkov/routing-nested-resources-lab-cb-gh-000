7feb07ec163f8f420c666e52550635fde63af922
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-13 20:31:09 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,11 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @artist = Artist.find(params[:artist_id])
       if @songs.nil?
-        redirect_to artists_url, flash[:alert] = "Artist not found.""
+        redirect_to artists_url, flash[:alert] = "Artist not found."
+      else
+        @songs = @artist.songs
       end
     else
       @songs = Song.all
